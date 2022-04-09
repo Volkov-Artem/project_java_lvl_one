a5a1756f3efa1af6667f28b43c2e64771ebcb96c
Artyom <Artyomvolkov@mail.ru>
2022-04-09 17:15:48 +1000

    NewchangesForJavaWork2Array

--- src/Work2Array.java
+++ src/Work2Array.java
@@ -1,12 +1,19 @@
+import java.sql.SQLOutput;
 import java.util.Arrays;
 
 public class Work2Array {
 
     public static void main(String[] args) {
+        int [] array = {15,2,2,5,5,5};
         changeNumber();
         arrayPush();
         multiArray();
         twoDimensionalArray();
+        maxAndMin();
+        System.out.println(checkBalance(array));
+        System.out.println("______________________");
+
+
     }
 
     public static void changeNumber() {
@@ -47,21 +54,53 @@ public class Work2Array {
         }
         System.out.println(Arrays.toString(array));
     }
-    public static void twoDimensionalArray(){
-        int [][] array = new int[5][5];
+    public static void twoDimensionalArray(){    //заполнение массива по диагонали при помощи вложенных циклов
+        int [][] array = new int[9][9];
+
         for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length; j++) {
-                if(i == j){
+            for (int j = 0, g = array.length-1; j < array.length; j++ , g--) {
+                if(i == j|| i==g){
                     array[i][j] = 1;
 
                 }
-                System.out.print(array[i][j]);
+                System.out.print(array[i][j]+" " );
 
             }
             System.out.println();
         }
 
-       // System.out.println(Arrays.deepToString(array));
+
+    }
+    public static void maxAndMin (){   // находи минимальгый и максимальный элемент массива
+        int [] arr = {1,3,4,-5,6,7,5,4,9};
+        int max = Integer.MIN_VALUE;
+        int min  = Integer.MAX_VALUE;
+        for (int i = 0 , j = arr.length-1; i < arr.length; i++, j--) {
+            if (max < arr[i]){
+                max = arr[i];
+            }
+            if (min > arr[j]){
+                min = arr[j];
+            }
+        }
+        System.out.println("MAX = "+ max + " MIN = "+ min);
+
+    }
+    public static boolean checkBalance (int[] arr) { // находим балансм между двумя половинками массива
+        int a = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int b = 0;
+            a += arr[i];    // левая
+            for (int k = i+1; k < arr.length; k++) {
+                b += arr[k]; //правая
+            }
+            if(a == b){ return true; // если есть баланс
+            }
+        }
+        return false;
     }
 
+
+
 }
+
